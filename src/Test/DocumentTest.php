e0f100ca109bfd38916bdf6b2157d34b2817504b
Christoph Massmann <c.massmann@vianetz.com>
2021-05-28 08:36:43 +0200

    added unit test for document class
    Signed-off-by: Christoph Massmann <c.massmann@vianetz.com>

---
+++ src/Test/DocumentTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @section LICENSE
+ * This file is created by vianetz <info@vianetz.com>.
+ * The code is distributed under the GPL license.
+ *
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@vianetz.com so we can send you a copy immediately.
+ *
+ * @package     Vianetz\Pdf
+ * @author      Christoph Massmann, <cm@vianetz.com>
+ * @link        https://www.vianetz.com
+ * @copyright   Copyright (c) since 2006 vianetz - Dipl.-Ing. C. Massmann (https://www.vianetz.com)
+ * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU GENERAL PUBLIC LICENSE
+ */
+
+namespace Vianetz\Pdf\Test;
+
+use PHPUnit\Framework\TestCase;
+use Vianetz\Pdf\Model\Document;
+
+final class DocumentTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    public function testSetPdfAttachmentFile()
+    {
+        $document = new Document();
+        $document->setPdfAttachmentFile('attachment1.pdf');
+
+        $this->assertEquals('attachment1.pdf', $document->getPdfAttachmentFile());
+    }
+}
